742b6185ab3e36c3b809ab71c2f18e5d2db76d37
Petra Volkova <volkovapetra17@gmail.com>
2024-11-08 09:44:30 +0100

    dalsipokus

--- scriptukol5.js
+++ scriptukol5.js
@@ -1,18 +1,12 @@
 const jmeno = prompt("Zadejte jmeno").trim()
 const prijmeni = prompt("Zadejte prijmeni").trim()
-
+const domain = "fit.cvut.cz"
 const castJmena = jmeno.slice(0, 3).toLowerCase()
 const castPrijmeni = prijmeni.slice(0, 5).toLowerCase()
-const email = castPrijmeni + castJmena + '@fit.cvut.cz'
 
-const atIndex = email.indexOf('@')
-const parsedEmail = {
-  
-  domain: email.slice(atIndex + 1),
-}
 
 document.body.innerHTML =
-  '<p>Email: ' + jmeno + '@' + parsedEmail.domain + '</p>';
+  '<p>Email: ' + castJmena + castPrijmeni + '@' + domain + '</p>';
 
 
 
